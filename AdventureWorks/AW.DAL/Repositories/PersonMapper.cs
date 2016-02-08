5b06614f26d85f9ea4457eadf9baa49363605889
ronlemire2 <ronlemire2@gmail.com>
2016-02-08 13:27:52 -0800

    Refactoring.

--- AdventureWorks/AW.DAL/Repositories/PersonMapper.cs
+++ AdventureWorks/AW.DAL/Repositories/PersonMapper.cs
@@ -20,10 +20,23 @@ namespace AW.DAL.Repositories {
             return dalPeople;
         }
 
+        // TODO: Use AutoMapper
         public static PersonPOCO MapOneEFtoDAL(Person efPerson) {
             PersonPOCO dalPersonPOCO = new PersonPOCO();
 
             dalPersonPOCO.BusinessEntityID = efPerson.BusinessEntityID;
+            dalPersonPOCO.PersonType = efPerson.PersonType;
+            dalPersonPOCO.NameStyle = efPerson.NameStyle.ToString();
+            dalPersonPOCO.Title = efPerson.Title;
+            dalPersonPOCO.FirstName = efPerson.FirstName;
+            dalPersonPOCO.MiddleName = efPerson.MiddleName;
+            dalPersonPOCO.LastName = efPerson.LastName;
+            dalPersonPOCO.Suffix = efPerson.Suffix;
+            dalPersonPOCO.EmailPromotion = efPerson.EmailPromotion.ToString();
+            dalPersonPOCO.AdditionalContactInfo = efPerson.AdditionalContactInfo;
+            dalPersonPOCO.Demographics = efPerson.Demographics;
+            dalPersonPOCO.rowguid = efPerson.rowguid.ToString();
+            dalPersonPOCO.ModifiedDate = efPerson.ModifiedDate.ToString();
             dalPersonPOCO.CrudState = CrudState.Unchanged;
 
             return dalPersonPOCO;
@@ -36,7 +49,19 @@ namespace AW.DAL.Repositories {
         public static Person MapOneDALtoEF(PersonPOCO dalPersonPOCO) {
             Person efPerson = new Person();
 
-            efPerson.BusinessEntityID = efPerson.BusinessEntityID;
+            efPerson.BusinessEntityID = dalPersonPOCO.BusinessEntityID;
+            efPerson.PersonType = dalPersonPOCO.PersonType;
+            efPerson.NameStyle = bool.Parse(dalPersonPOCO.NameStyle);
+            efPerson.Title = dalPersonPOCO.Title;
+            efPerson.FirstName = dalPersonPOCO.FirstName;
+            efPerson.MiddleName = dalPersonPOCO.MiddleName;
+            efPerson.LastName = dalPersonPOCO.LastName;
+            efPerson.Suffix = dalPersonPOCO.Suffix;
+            efPerson.EmailPromotion = int.Parse(dalPersonPOCO.EmailPromotion);
+            efPerson.AdditionalContactInfo = dalPersonPOCO.AdditionalContactInfo;
+            efPerson.Demographics = dalPersonPOCO.Demographics;
+            efPerson.rowguid = Guid.Parse(dalPersonPOCO.rowguid);
+            efPerson.ModifiedDate = DateTime.Parse(dalPersonPOCO.ModifiedDate);
 
             return efPerson;
         }
